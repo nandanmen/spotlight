2c3bc34d3d63f6b38bbe387d9d70d216d471bb2d
Narendras <n.syahrasyad.n@gmail.com>
2019-08-07 09:52:35 -0700

    Fixed math tests

--- src/middlewares/math/__tests__/usage.ts
+++ src/middlewares/math/__tests__/usage.ts
@@ -2,16 +2,19 @@ import mathMiddleware from '..'
 
 describe('math middleware', () => {
   describe('usage', () => {
+    const getResults = (input: string) =>
+      mathMiddleware(input).map(result => result.value)
+
     it('returns empty array if input is not a math expression', () => {
-      expect(mathMiddleware('abc')).toBeEmpty()
+      expect(getResults('abc')).toBeEmpty()
     })
 
     it('returns empty array if function has no arguments', () => {
-      expect(mathMiddleware('sin()')).toBeEmpty()
+      expect(getResults('sin()')).toBeEmpty()
     })
 
     it('performs simple arithmetic', () => {
-      expect(mathMiddleware('3 + 3')).toContain(6)
+      expect(getResults('3 + 3')).toContain(6)
     })
 
     describe('bedmas rules', () => {
@@ -21,7 +24,7 @@ describe('math middleware', () => {
         ${'(3 + 3) * 4'}     | ${24}
         ${'3 + (3 * 4 / 2)'} | ${9}
       `(`given $input returns $result`, ({ input, result }) => {
-        expect(mathMiddleware(input)).toContain(result)
+        expect(getResults(input)).toContain(result)
       })
     })
 
@@ -31,12 +34,12 @@ describe('math middleware', () => {
         ${'1/2*((-2*(1+2))*10)'}      | ${-30}
         ${'1800/10*((12-6)+(24-12))'} | ${3240}
       `(`given $input returns $result`, ({ input, result }) => {
-        expect(mathMiddleware(input)).toContain(result)
+        expect(getResults(input)).toContain(result)
       })
     })
 
     it('evaluates functions', () => {
-      expect(mathMiddleware('sin(max(450, 451))')).toContain(Math.sin(451))
+      expect(getResults('sin(max(450, 451))')).toContain(Math.sin(451))
     })
   })
 })
